ed76890ba0d0a5aa07e53753803b5e9e1383087c
Andrey Buchevskiy <aabuchevsk@mts.ru>
2024-04-09 09:19:01 +0300

    Initial Commit

---
+++ LinkedAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LinkedAnimation
+//
+//  Created by Бучевский Андрей on 09.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
